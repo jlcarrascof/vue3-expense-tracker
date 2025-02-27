df40ace6b6831712297f2aa60063a75eb1933b4d
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-02-26 21:57:58 -0400

    add test route and initialize server in backend

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,14 @@ const app = express()
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+// Test route
+app.get('/api', (req, res) => {
+    res.send({ message: 'API working correctly' })
+});
+
+// Init server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
