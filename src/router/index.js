9590ed13aeb117a5769404dea1eb12e2a6e56c24
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-05 09:47:23 -0400

    add route definitions for Login, SignUp, and Dashboard views

--- src/router/index.js
+++ src/router/index.js
@@ -3,3 +3,15 @@ import Login from '../views/Login.vue'
 import SignUp from '../views/SignUp.vue'
 import Dashboard from '../views/Dashboard.vue'
 
+const routes = [
+    { path: '/', component: Login },
+    { path: '/signup', component: SignUp },
+    { path: '/dashboard', component: Dashboard },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
