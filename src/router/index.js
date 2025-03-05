e989ff49a620b382e26bbc7050b583633cf542e3
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-05 09:46:23 -0400

    add router configuration with Login, SignUp, and Dashboard views

---
+++ src/router/index.js
@@ -0,0 +1,5 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
+import Dashboard from '../views/Dashboard.vue'
+
