79aa2c2c4efbc83b46faf4e491fb7a0fd74da2ba
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-07 19:56:17 -0400

    refactor Login.vue to use script setup and update password placeholder text

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,10 @@
-<script>
-    import { ref } from "vue"
-    const showPassword = ref(false)
+<script setup>
+    import { ref } from "vue";
+    const showPassword = ref(false);
 </script>
 
 <template>
     <div class="h-screen flex">
-        <!-- Sección Izquierda: Formulario de Login -->
         <div class="w-1/2 flex items-center justify-center bg-white">
             <div class="p-8 rounded-lg shadow-lg w-96">
                 <h2 class="text-2xl font-bold mb-4 text-center text-black">Welcome Back</h2>
@@ -17,7 +16,7 @@
                     <div class="relative">
                         <input
                             :type="showPassword ? 'text' : 'password'"
-                            placeholder="Password"
+                            placeholder="Max 10 chars"
                             class="w-full p-2 rounded bg-gray-200 border pr-10"
                         />
                         <button
@@ -30,20 +29,18 @@
                         </button>
                     </div>
 
-                    <button class="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700 transition">
+                    <button class="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700 transition mt-4">
                         Log in
                     </button>
                     <p class="text-center mt-4">
                         Don't have an account?
                         <router-link to="/signup" class="text-green-600 font-semibold hover:underline">
-                            Sign Up here
+                            SignUp
                         </router-link>
                     </p>
                 </form>
             </div>
         </div>
-
-        <!-- Sección Derecha: Espacio para Imagen o Gráficos -->
         <div class="w-1/2 flex items-center justify-center bg-green-100">
             <img src="../assets/vue-dashboard.jpg" alt="Vue Dashboard" class="w-3/4">
         </div>
