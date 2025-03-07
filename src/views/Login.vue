1dd68005b9d22f960f32f148dc23d75f845eaeb8
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-07 12:53:03 -0400

    fix image path in Login view for correct asset loading

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,6 @@
 
       <!-- Sección Derecha: Espacio para Imagen o Gráficos -->
       <div class="w-1/2 flex items-center justify-center bg-green-100">
-          <img src="@/assets/vue-dashboard.png" alt="Vue Dashboard" class="w-3/4">
+          <img src="../assets/vue-dashboard.jpg" alt="Vue Dashboard" class="w-3/4">
       </div>
 </template>
