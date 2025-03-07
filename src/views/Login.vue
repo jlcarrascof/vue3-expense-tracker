c723ecf97964160f893076f814d1474c8da7dd71
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-07 12:51:36 -0400

    add image to Login view and restructure layout for improved design

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,7 @@
 <template>
-    <div class="min-h-screen flex items-center justify-center bg-gray-100">
-      <div class="bg-white p-8 rounded-lg shadow-lg w-96">
-        <h2 class="text-2xl font-bold mb-4 text-center">Iniciar Sesión</h2>
-        <form>
-          <input type="email" placeholder="E-mail" class="w-full p-2 border rounded mb-3" />
-          <input type="password" placeholder="Password" class="w-full p-2 border rounded mb-3" />
-          <button class="w-full bg-green-600 text-white py-2 rounded">Ingresar</button>
-        </form>
-        <p class="text-center mt-4">
-          ¿No tienes cuenta? <router-link to="/signup" class="text-green-600">Regístrate aquí</router-link>
-        </p>
+
+      <!-- Sección Derecha: Espacio para Imagen o Gráficos -->
+      <div class="w-1/2 flex items-center justify-center bg-green-100">
+          <img src="@/assets/vue-dashboard.png" alt="Vue Dashboard" class="w-3/4">
       </div>
-    </div>
 </template>
