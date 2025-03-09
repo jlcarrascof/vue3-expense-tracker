d2aff87df9603d97c1aeb3325c4165b71163d6bd
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-09 14:42:08 -0400

    add header and description to SignUp view for improved user guidance

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -17,7 +17,8 @@
       <!-- Sección Izquierda: Formulario de Registro -->
       <div class="w-1/2 flex items-center justify-center bg-white">
         <div class="p-8 rounded-lg shadow-lg w-96">
-
+          <h2 class="text-2xl font-bold mb-4 text-center text-black">Create Account</h2>
+          <p class="text-gray-500 text-center mb-6">Please enter your details to sign up</p>
         </div>
       </div>
    </div>
