ba29b32a5f9145eff262ef872d6872603c927ea9
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-18 10:33:08 -0400

    simplify server startup log message in server.js

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,6 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err))
 
-// Init server
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
