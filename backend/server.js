f9009f60d8631a6da98274372b579ace5124e3ce
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-18 10:27:55 -0400

    add environment configuration and integrate dotenv in server setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+require("dotenv").config()
 
 // Middleware
 app.use(cors())
