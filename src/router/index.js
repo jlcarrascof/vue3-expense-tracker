113e2af7ebc1baf363ddbce4f72fa5b3d10cc0c2
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-25 11:52:27 -0400

    feat: add route names and protect dashboard route with authentication meta

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,17 @@ import SignUp from '../views/SignUp.vue'
 import Dashboard from '../views/Dashboard.vue'
 
 const routes = [
-    { path: '/', component: Login },
-    { path: '/signup', component: SignUp },
-    { path: '/dashboard', component: Dashboard },
+    { path: '/', name: 'Login', component: Login },
+    { path: '/signup', name: 'SignUp', component: SignUp },
+    { path: '/dashboard', name: 'Dashboard', component: Dashboard, meta: { requiresAuth: true } }, // Protected route
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+// 🚀 Protect private routes.
+
 
 export default router
