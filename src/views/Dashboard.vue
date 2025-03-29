a112642e802c6eed6bb80f0e611c811c4bcf7ace
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-29 13:09:39 -0400

    refactor: comment out unused components in App.vue and Dashboard.vue for cleaner code

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="flex h-screen">
     <!-- Sidebar -->
+    <!--
     <Sidebar />
+    -->
     <div class="flex-1 flex flex-col">
       <!-- Header -->
       <Header />
       <div class="flex flex-1">
         <!-- Financial Info -->
+        <!--
         <FinancialInfo />
+        -->
         <!-- Charts -->
+        <!--
         <Charts />
+        -->
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+  // import Sidebar from './Sidebar.vue';
+  import Header from '../components/Header.vue';
+  // import FinancialInfo from './FinancialInfo.vue';
+  // import Charts from './Charts.vue';
+</script>
