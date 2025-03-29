f4fa3a5427396b1a634e53a34bc309587eb544ec
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-29 18:54:27 -0400

    feat: update Sidebar component styles for improved aesthetics and user experience

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -10,10 +10,10 @@
 </script>
 
 <template>
-  <aside class="bg-green-700 text-white w-64 min-h-screen p-5">
+  <aside class="bg-white border-r text-gray-800 font-bold w-64 min-h-screen p-5">
     <!-- Perfil -->
     <div class="flex flex-col items-center mb-6">
-      <img src="@/assets/profile.jpg" alt="User Avatar" class="w-16 h-16 rounded-full mb-2">
+      <img src="../assets/profile.jpg" alt="User Avatar" class="w-16 h-16 rounded-full mb-2">
       <h2 class="text-lg font-semibold">Javier Martínez</h2>
     </div>
 
@@ -22,7 +22,7 @@
       <ul>
         <li v-for="item in menuItems" :key="item.path" class="mb-4">
           <router-link :to="item.path" class="flex items-center p-2 rounded-lg"
-            :class="{ 'bg-green-900': $route.path === item.path }">
+            :class="{ 'bg-green-900 text-white': $route.path === item.path }">
             <span class="mr-3">{{ item.icon }}</span>
             {{ item.label }}
           </router-link>
