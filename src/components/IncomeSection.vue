26cb3ae989962fc3573ee9724c1f86092ca3b7a8
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-04-06 19:24:08 -0400

    style: add IncomeSection component to display income sources

---
+++ src/components/IncomeSection.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { ref, onMounted } from 'vue'
+    import Chart from 'chart.js/auto'
+
+    const incomes = ref([
+    { id: 1, name: 'Salary', date: '12th Feb 2025', amount: 12000, icon: '💼' },
+    { id: 2, name: 'Interest from Savings', date: '13th Jan 2025', amount: 9800, icon: '🏦' },
+    { id: 3, name: 'E-commerce Sales', date: '11th Jan 2025', amount: 11900, icon: '🛒' },
+    { id: 4, name: 'Graphic Design', date: '10th Jan 2025', amount: 10500, icon: '🎨' },
+    { id: 5, name: 'Affiliate Marketing', date: '8th Jan 2025', amount: 8000, icon: '🤝' },
+    ])
+
+</script>
+
+<template>
+
+</template>
