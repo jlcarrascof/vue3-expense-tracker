2e6a7e3bb2805efd26553b2b6bef684885f92371
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-04-10 14:12:26 -0400

    style: add IncomeChart component with initial setup for chart visualization

---
+++ src/components/IncomeChart.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import { onMounted, ref } from 'vue'
+    import { Chart, registerables } from 'chart.js'
+
+    // Necessary registration of chart.js components
+    Chart.register(...registerables)
+
+    const chartCanvas = ref(null)
+
+
+
+</script>
+
+<template>
+
+</template>
